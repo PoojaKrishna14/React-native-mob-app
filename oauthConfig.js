e078806bb28c3d1bdce5fcd1119db1126ee2bf64
Pooja Krishna <107520967+PoojaKrishna14@users.noreply.github.com>
2024-03-12 18:12:28 +0530

    Adding config files
    only external file

---
+++ oauthConfig.js
@@ -0,0 +1,23 @@
+export default {
+  grant_type: "password",
+
+  client_id:
+    "3MVG9ZUGg10Hh224dLrl9wXsHAMsIEMCA2YdbSy4TTWzag18glt0P9h3gNxmBvrXfnFE4LPkJ6I.Z8KjVEafI",
+
+  client_secret:
+    "1F72B06A4D12E374B8B4857F60BC02BB2C9536E9CB6BD210C2C72AE2405E6066",
+
+  username: "santhosh-d1y1@force.com.uat",
+
+  password: "Pass4LV#2108",
+  redirectUrl:
+    "https://https://languageveda--uat.sandbox.my.salesforce.com/services/oauth2/token",
+
+  authorizationUrl: "https://login.salesforce.com/services/oauth2/token",
+
+  tokenUrl: "https://login.salesforce.com/services/oauth2/token",
+
+  scope: "full refresh_token", // Adjust the scope as needed
+
+  password: "Pass4LV#2108",
+};
